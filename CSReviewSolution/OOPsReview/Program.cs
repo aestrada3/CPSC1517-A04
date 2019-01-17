715ac92944e811865e46446898d446fdb3b0a6d2
aestrada3 <aestrada3@studentmail.nait.ca>
2019-01-16 23:44:08 -0700

    Update Program.cs

--- CSReviewSolution/OOPsReview/Program.cs
+++ CSReviewSolution/OOPsReview/Program.cs
@@ -11,6 +11,29 @@ namespace OOPsReview
     {
         static void Main(string[] args)
         {
+            int Rounds;
+            string PlayerOne;
+            string PlayerTwo;
+            int PlayerOneScore;
+            int PlayerTwoScore;
+
+
+            Console.WriteLine("Name of Player one:");
+            PlayerOne = Console.ReadLine();
+
+            Console.WriteLine("Name of Player two:");
+            PlayerTwo = Console.ReadLine();
+
+            Console.WriteLine("Enter the number of rounds to be played");
+            Rounds = Convert.ToInt32( Console.ReadLine());
+
+            for (;Rounds > 0;Rounds--)
+            {
+                PlayerOneScore = new Die();
+
+
+            }
+            
 
         }
     }
