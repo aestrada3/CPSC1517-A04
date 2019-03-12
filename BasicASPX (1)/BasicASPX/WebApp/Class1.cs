8a0f55bac4c081e23438582a77f7b0efea65bb46
aestrada3 <aestrada3@studentmail.nait.ca>
2019-03-12 16:49:44 -0600

    Changed

---
+++ BasicASPX (1)/BasicASPX/WebApp/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp
+{
+    public class Class1
+    {
+        private string _StreetAddress2;
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetAddress1 { get; set; }
+        public string StreetAddress2
+        {
+            get
+            {
+                return _StreetAddress2;
+            }
+            set
+            {
+                _StreetAddress2 = string.IsNullOrEmpty(value) ? null : value;
+            }
+
+        }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string EmailAddress { get; set; }
+        public Class1(string firstname, string lastname, string streetaddress1, string streetaddress2)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            StreetAddress1 = streetaddress1;
+            StreetAddress2 = streetaddress2;
+        }
+    }
+    }
---
+++ BasicASPX (1)/BasicASPX/WebApp/SamplePages/ContestEntry.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp.SamplePages
+{
+    public partial class ContestEntry : System.Web.UI.Page
+    {
+
+        public static List<Class1> ndcollection;
+        
+
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Message.Text = "";
+            if (!Page.IsPostBack)
+            {
+                ndcollection = new List<Class1>();
+            }
+        }
+
+        //public static List<GridViewCollection> newcollection;
+        
+
+        protected void Submit_Click1(object sender, EventArgs e)
+        {
+            //validate the data coming in
+            if (Page.IsValid)
+            {
+                //validate the user checking the terms
+                if(Terms.Checked)
+                {
+                    //yes: create/load Entry, add to List, display List
+                    string firstname = FirstName.Text;
+                    string lastname = LastName.Text;
+                    string streetaddress1 = StreetAddress1.Text;
+                    string streetaddress2 = StreetAddress2.Text;
+                    string city = City.Text;
+                    string postalcode = PostalCode.Text;
+                    string emailaddress = EmailAddress.Text;
+                    int province = Province.SelectedIndex;
+
+
+                    ndcollection.Add(new Class1(firstname, lastname, streetaddress1, streetaddress2));
+                }
+                else
+                {
+                    // no: message
+                    Message.Text = "You did not agree to the terms of this contest. Entry is denied.";
+                }
+                
+                
+            }
+        }
+
+        protected void Clear_Click(object sender, EventArgs e)
+        {
+            FirstName.Text = "";
+            LastName.Text = "";
+            StreetAddress1.Text = "";
+            StreetAddress2.Text = "";
+            City.Text = "";
+            PostalCode.Text = "";
+            EmailAddress.Text = "";
+            Province.SelectedIndex = 0;
+            Terms.Checked = false;
+
+        }
+    }
+}
